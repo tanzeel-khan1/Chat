ef6b753e9b397ec7e888168121ee839fa7924152
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-12-12 12:16:50 +0500

    ki

--- Backend/models/Conversation.js
+++ Backend/models/Conversation.js
@@ -7,6 +7,7 @@ import mongoose from "mongoose";
 const conversationSchema = new mongoose.Schema(
   {
     participants: [
+      
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",  // STRING hona chahiye
